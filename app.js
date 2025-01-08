d9a849b0e474097e7008e9d7260ebdd3bb23664e
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-01-08 15:41:43 +0100

    ADD: express and root route

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("ciao");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("Root");
+});
+
+
+app.listen(port, () => {
+    console.log(`Server port: ${port}`);
+});
