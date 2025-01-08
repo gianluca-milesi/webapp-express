f46f4aa73eaa80e4a2da33d9afc3a49f0461c2f9
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-01-08 15:56:34 +0100

    ADD: .env file and db connection

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 3000
 
 
 
 
 app.get("/", (req, res) => {
-    res.send("Root");
-});
+    res.send("Root")
+})
 
 
 app.listen(port, () => {
-    console.log(`Server port: ${port}`);
-});
+    console.log(`Server port: ${port}`)
+})
